0bd0529e85370814990ab387ed9debcae28d6802
siwenshao <146389391+siwenshao@users.noreply.github.com>
2025-02-16 02:02:57 -0800

    added advance and pause buttons, ran all tests, all passed! : D

--- app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/task/TaskFragment.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/task/TaskFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -63,6 +64,8 @@ public class TaskFragment extends Fragment {
         RecyclerView tasks = view.findViewById(R.id.taskRecyclerView);
         Button backButton = view.findViewById(R.id.backButton);
         Button endRoutineButton = view.findViewById(R.id.endRoutineButton);
+        ImageButton stopButton = view.findViewById(R.id.button_stop);
+        ImageButton advanceButton = view.findViewById(R.id.button_advance);
         TextView timeRemaining = view.findViewById(R.id.timeRemaining);
 
 
@@ -86,6 +89,13 @@ public class TaskFragment extends Fragment {
             public void onRoutineCompleted(int totalTime, String formattedTime) {
                 requireActivity().runOnUiThread(() -> timeRemaining.setText("Completed in: " + formattedTime));
             }
+
+            @Override
+            public void onPauseToggled(boolean isPaused) {
+                requireActivity().runOnUiThread(() -> {
+                    stopButton.setImageResource(isPaused ? R.drawable.pause : R.drawable.pause); //let them both be pause icon for now
+                });
+            }
         });
 
         totalTimer.start(); // Start the timer when the fragment loads
@@ -95,6 +105,8 @@ public class TaskFragment extends Fragment {
             totalTimer.stop();
         });
 
+        stopButton.setOnClickListener(v -> totalTimer.togglePause());
+        advanceButton.setOnClickListener(v -> totalTimer.advanceTime());
 
         return view;
     }
