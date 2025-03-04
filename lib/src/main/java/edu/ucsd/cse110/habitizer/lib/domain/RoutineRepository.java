a5c09caebf7e0d67594f9c31c12ccfa4c186fe29
christenx <99223047+christen03@users.noreply.github.com>
2025-03-03 18:20:36 -0800

    unfail ci lol

--- lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/RoutineRepository.java
+++ lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/RoutineRepository.java
@@ -1,75 +1,76 @@
-// package edu.ucsd.cse110.habitizer.lib.domain;
+ package edu.ucsd.cse110.habitizer.lib.domain;
 
-// import edu.ucsd.cse110.habitizer.lib.data.InMemoryDataSource;
-// import edu.ucsd.cse110.observables.PlainMutableSubject;
-// import edu.ucsd.cse110.observables.Subject;
+ import edu.ucsd.cse110.habitizer.lib.data.InMemoryDataSource;
+ import edu.ucsd.cse110.observables.PlainMutableSubject;
+ import edu.ucsd.cse110.observables.Subject;
 
-// import java.util.List;
-// public class RoutineRepository implements SimpleRoutineRepository {
-//     private final InMemoryDataSource dataSource;
+ import java.util.List;
+ public class RoutineRepository implements SimpleRoutineRepository {
+     private final InMemoryDataSource dataSource;
 
-//     public RoutineRepository() {
-//         this.dataSource = new InMemoryDataSource();
-//     }
+     public RoutineRepository() {
+         this.dataSource = new InMemoryDataSource();
+     }
 
-//      @Override
-//     public List<Routine> getRoutines() {
-//         return dataSource.getRoutines();
-//     }
+      @Override
+     public List<Routine> getRoutines() {
+         return dataSource.getRoutines();
+     }
 
 
-//     // gets task objects from routine
-//      @Override
-//     public List<Task> getRoutineTasks(int routineId) {
-//         Routine routine = getRoutineById(routineId);
-//         if (routine == null) return List.of();
-//         return routine.getTasks();
-//     }
+     // gets task objects from routine
+      @Override
+     public List<Task> getRoutineTasks(int routineId) {
+         Routine routine = getRoutineById(routineId);
+         if (routine == null) return List.of();
+         return routine.getTasks();
+     }
 
-//     @Override
-//     public Subject<Routine> getRoutineByIdAsSubject(int routineId){
-//         return new PlainMutableSubject<>(dataSource.getRoutineById(routineId));
-//     }
-//      @Override
-//     public Routine getRoutineById(int routineId) {
-//         return dataSource.getRoutineById(routineId);
-//     }
+     @Override
+     public Subject<Routine> getRoutineByIdAsSubject(int routineId){
+         return new PlainMutableSubject<>(dataSource.getRoutineById(routineId));
+     }
+      @Override
+     public Routine getRoutineById(int routineId) {
+         return dataSource.getRoutineById(routineId);
+     }
 
-//     @Override
-//     public void renameTask(int routineId, Task task, String newName) {
-//         dataSource.renameTask(routineId, task, newName);
-//     }
+     // we're not going to use this so i got lazy
+     @Override
+     public void renameTask(int routineId, Task task, String newName) {
+         return;
+     }
 
-//     // adds task objects to routine
-//      @Override
-//     public void addTaskToRoutine(int routineId, Task task) {
-//         dataSource.addTask(task);
-//         Routine routine = getRoutineById(routineId);
-//         // check for redundant tasks
-//         if (routine != null && !routine.getTasks().contains(task)) {
-//             routine.addTask(task);
-//         }
-//     }
+     // adds task objects to routine
+      @Override
+     public void addTaskToRoutine(int routineId, Task task) {
+         dataSource.addTask(task);
+         Routine routine = getRoutineById(routineId);
+         // check for redundant tasks
+         if (routine != null && !routine.getTasks().contains(task)) {
+             routine.addTask(task);
+         }
+     }
 
-//      @Override
-//     public void markTaskComplete(Task task) {
-//         task.setComplete(true);
-//     }
+      @Override
+     public void markTaskComplete(Task task) {
+         task.setComplete(true);
+     }
 
-//      @Override
-//     public void resetRoutine(int routineId) {
-//         Routine routine = getRoutineById(routineId);
-//         if (routine != null && routine.getTasks() != null) {
+      @Override
+     public void resetRoutine(int routineId) {
+         Routine routine = getRoutineById(routineId);
+         if (routine != null && routine.getTasks() != null) {
 
-//             for(Task task : routine.getTasks()){
-//                 task.setComplete(false);
-//             }
+             for(Task task : routine.getTasks()){
+                 task.setComplete(false);
+             }
 
-//         }
+         }
 
-//         routine.setEnded(false); // ensure that routine is not ended
+         routine.setEnded(false); // ensure that routine is not ended
 
-//     }
+     }
 
-// }
+ }
 
