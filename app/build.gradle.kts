83e2b93e52111df8ff8d88bd43a4fa3150c4a28e
katrinab2727 <katrinab2727@gmail.com>
2025-03-04 17:40:14 -0800

    Dao tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,12 +40,14 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     implementation(project(":lib"))
     implementation(project(":observables"))
     implementation("androidx.room:room-runtime:2.6.1")
 
     annotationProcessor("androidx.room:room-compiler:2.6.1")
     testImplementation(libs.junit4)
+    testImplementation("org.mockito:mockito-core:4.8.0")
 
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.androidx.test.ext.espresso.core)
